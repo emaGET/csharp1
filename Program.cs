5cf9bd2b29edea05cf16c34b85b0da6e207bda4a
emaGET <emanuelepini@getacademy.no>
2026-05-06 11:13:00 +0200

    tried some things

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace ConsoleApplication1_csharp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            //console.WriteLine("Ayo World");
+            //let's remember the capital letters, or we get error 10:
+            //   Program.cs(10, 13): [CS0103] The name 'console' does not exist in the current context
+            
+            // Console.WriteLine('Hi world');
+            // also does not work because we need " and not '
+            
+            Console.WriteLine("Congrats!");
+            Console.WriteLine("");
+            // Console.WriteLine("First lines of code achieved!");
+            Console.WriteLine("First lines of code achieved!");
+            Console.Write("And this is the second (more like the fifth) line");
+
+            // end of part 1
+        }
+    }
+}
