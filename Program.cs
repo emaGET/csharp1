483fe3d4bf9914a6649122ecd156b31923b310d2
emaGET <emanuelepini@getacademy.no>
2026-05-08 12:29:05 +0200

    var, bool, if and else try.

--- Program.cs
+++ Program.cs
@@ -6,34 +6,36 @@ namespace ConsoleApplication1_csharp1
     {
         public static void Main(string[] args)
         {
+            var city = "Tromsø";
+            int inhabitants = 79604;
             
-            Console.WriteLine("Hello World!");
-            
-            //console.WriteLine("Ayo World");
-            //let's remember the capital letters, or we get error 10:
-            //   Program.cs(10, 13): [CS0103] The name 'console' does not exist in the current context
-            
-            // Console.WriteLine('Hi world');
-            // also does not work because we need " and not '
-            
-            Console.Write("Congrats!");
-            Console.WriteLine("");
-            // Console.WriteLine("First lines of code achieved!");
-            Console.WriteLine("First lines of code achieved!");
-            Console.WriteLine("And this is the second (more like the fifth) line");
-
-            // end of part 1
-
             Console.WriteLine("Hello World, from See Sharp! Remember your glasses");
+            Console.WriteLine($"We are {inhabitants} here in {city}.");
             Console.Write("What is your name? ");
             
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name + "! Welcome to the optometrist.");
+                // Console.WriteLine("Hello " + name + "! Welcome to the optometrist.");
+            Console.WriteLine($"Hello {name}! Welcome to the optometrist.");
+            
             Console.WriteLine("What is you age?");
             int age = Convert.ToInt32(Console.ReadLine());
             int yearBorn = 2026 - age;
-            // Console.WriteLine("Your age is " + age + ", then you were born in " + yearBorn + " if you hadn't a birthday yet.");
-            Console.WriteLine($"Your age is {age}, then you were born in {yearBorn}, if you hadn't a birthday yet. Otherwise, {yearBorn-1}");
+                // Console.WriteLine("Your age is " + age + ", then you were born in " + yearBorn + " if you hadn't a birthday yet.");
+            Console.WriteLine($"Your age is {age}, then you were born in {yearBorn}, if you had your birthday already. Otherwise, {yearBorn-1}");
+            
+                // let's begin with bool!
+            
+            Console.WriteLine("Have you had your birthday this year? (y/n) ");
+            string bdAlready = Console.ReadLine();
+            bool hadBday  = bdAlready == "y";
+            if (hadBday)
+            {
+                Console.WriteLine($"well then you must be from {yearBorn}");
+            }
+            else
+            {
+                Console.WriteLine($"then you are from {yearBorn-1}");
+            }
             
         }
     }
